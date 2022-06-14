4eb6d5d6844950e98caac009fbb020695da10da4
mklochkov <m.klo4koff@yandex.ru>
2022-06-14 18:01:55 +0300

    start BERT implementation

---
+++ bert.py
@@ -0,0 +1,56 @@
+
+import torch
+
+import torch.nn as nn
+from torch.functional import F
+from utils import scaled_dot_product
+
+class MultiHeadAttention(nn.Module):
+
+
+    def __init__(self, in_dim, emb_dim, num_heads):
+        super().__init__()
+        assert emb_dim % num_heads == 0, 'Incorrect behaviour!'
+        # X (B, T, d_in) -> X (B, T, d_in)
+        self.emb_dim = emb_dim
+        self.num_heads = num_heads
+        self.head_dim = emb_dim // num_heads
+        # use one matrix
+        self.QKV = nn.Linear(in_dim, emb_dim * 3)
+        self.O_proj = nn.Linear(emb_dim, emb_dim)
+
+
+    # need xavier initialization Uniform a = (sqrt(6/(f_in + f_out)))
+    def _reset(self):
+        nn.init.xavier_uniform_(self.QKV.weight)
+        self.QKV.bias.data.fill_(0)
+        nn.init.xavier_normal_(self.O_proj.weight)
+        self.O_proj.bias.data.fill_(0)
+
+
+    def forward(self, x, mask=None, return_attention=False):
+        B, T, d_in = x.size()
+        qkv = self.QKV(x)
+        # separate Q, K, V for each head
+        qkv = qkv.reshape(B, T, self.num_heads, 3*self.head_dim)
+        # B x H x T x 3*d_h -> B x H x T x d_h
+        qkv = qkv.permute(0,2,1,3)
+        # each B x H x T x d_h
+        q, k, v = qkv.chunk(3, dim=-1)
+        values, attention = scaled_dot_product(q, k, v, mask)
+        # B x T x H x d_h
+        values = values.permute(0, 2, 1, 3)
+        # B x T x H * d_h -> B x T x d_out -> new dimentions
+        values = values.reshape(B, T, self.emb_dim)
+        # emb_dim x emd_dim values -> B x T x emb_dim
+        out = self.O_proj(values)
+        if return_attention:
+            return out, attention
+
+        return out
+
+
+if __name__ == '__main__':
+    mheadatt = MultiHeadAttention(100, emb_dim=80, num_heads=8)
+    X = torch.randn(8, 72, 100)
+    out = mheadatt.forward(X)
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
-import pathlib
+import pathlib, torch
 
+import numpy as np
+from torch.functional import F
 from typing import Union
 from pathlib import Path
 
@@ -16,3 +18,24 @@ def check_path(path: Union[str, pathlib.Path], type='file'):
 
     return path
 
+
+def softmax_own(x):
+    x_mean = x.mean(dim=-1, keepdims=True)
+    x_exp = torch.exp(x-x_mean)
+    return x_exp / torch.sum(x_exp, dim=-1, keepdim=True)
+
+
+def scaled_dot_product(q, k, v, mask=None):
+    # BxTxT
+    attn_logits = torch.matmul(q, k.transpose(-2, -1))
+    # BxTxT
+    attn_logits /= np.sqrt(q.shape[2])
+    if mask is not None:
+        # BxTxT first is query second keys (sum by keys)
+        attn_logits = attn_logits.masked_fill(mask==0, -9e15) # -inf -> softmax 0
+    attention = F.softmax(attn_logits, dim=-1)
+    # attention = softmax_own(attn_logits)
+    # BxTxT and BxTxd_v -> BxTxd_v
+    values = torch.matmul(attention, v)
+
+    return values, attention
